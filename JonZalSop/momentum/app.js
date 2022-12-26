7d1f05db41406f70ee5eeef64ce10b01745bbc47
minsop <minsoplee2015@gmail.com>
2022-12-26 22:03:30 +0900

    login & greeting
    이름을 쓰면 인사를 해준다? ㅁㅊㄷㅁㅊㄷㅁㅊㄷㅁㅊㄷㅁㅊㄷㅁㅊㄷ
    로깔 스토리지

--- JonZalSop/momentum/app.js
+++ JonZalSop/momentum/app.js
@@ -1,56 +1,30 @@
-// alert("hi");
-console.log(1000);
-console.log("10sadasd");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
 
-const a = 5;
-const b = 2;
-console.log(a + b);
-console.log(a * b);
-console.log(a/b);
-
-let myname = "kingJung"
-console.log(myname)
-myname = true
-console.log(myname)
-
-const amIFat = null;
-let something;
-console.log(amIFat);
-console.log(something);
-
-
-const player = {
-  name : "nico",
-  sayHello: function(otherPersonName) {
-      console.log("hello " + otherPersonName)
-  },
-
-};
-
-player.sayHello("아아아아")
-
-function minusFive(potato){
-  console.log(potato - 5);
+function onLoginSubmit(event){
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;  
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreeting(username) 
 }
 
-minusFive(5,6,6,7,9,10)
-
-function calculateKrAge(ageOfForeigner){
-  return ageOfForeigner + 2;
+function paintGreeting(username){
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = "Hello " + username;  
+  //백틱키
+  // greeting.innerText = `Hello ${username}`;  
 }
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-const age = parseInt(prompt("How old are you?"));
-
-if(isNaN(age) || age < 0){
-  console.log("확인좀")
-}else if (age < 18){
-  console.log("머리에 피가  안말라")
-}else if (age >= 18 && age <= 50){
-  console.log("마셔 마셔")
-}else if (age > 50 && age <= 80){
-  console.log("건강 챙기쇼")
-}else if (age > 80){
-  console.log("뭐든 해")
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+}else{
+  paintGreeting(savedUsername)
 }
